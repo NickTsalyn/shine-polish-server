878350418e2e27087e3bd910b5f471213bc88298
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-05-08 12:18:07 +0300

    Test booking-model created

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookingsService } from './bookings.service';
+import { CreateBookingDto } from './dto/create-booking.dto';
 
 @Controller('bookings')
-export class BookingsController {}
+export class BookingsController {
+    constructor(private bookingsService: BookingsService) {}
+
+    @Post()
+    create(@Body() bookingDto: CreateBookingDto) {
+        return this.bookingsService.createBooking(bookingDto);
+    }
+
+    @Get()
+    allBookings() {
+        return this.bookingsService.getAllBooking();
+    }
+}
--- src/bookings/bookings.module.ts
+++ src/bookings/bookings.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { BookingsController } from './bookings.controller';
 import { BookingsService } from './bookings.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Booking, BookingSchema } from './bookings.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Booking.name,
+        schema: BookingSchema,
+      },
+    ]),
+  ],
   controllers: [BookingsController],
-  providers: [BookingsService]
+  providers: [BookingsService],
 })
 export class BookingsModule {}
