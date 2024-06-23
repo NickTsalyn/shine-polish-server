02365dc23e65b0e636d08c3c6ba9a46f49a5931f
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-06-23 20:06:53 +0300

    updated Booking for authorised and unauthorised users

--- src/bookings/bookings.controller.ts
+++ src/bookings/bookings.controller.ts
@@ -1,18 +1,18 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { BookingsService } from './bookings.service';
-import { CreateBookingDto } from './dto/create-booking.dto';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { BookingsService } from "./bookings.service";
+import { CreateBookingDto } from "./dto/create-booking.dto";
 
-@Controller('bookings')
+@Controller("bookings")
 export class BookingsController {
-    constructor(private bookingsService: BookingsService) {}
+  constructor(private bookingsService: BookingsService) {}
 
-    @Post()
-    create(@Body() bookingDto: CreateBookingDto) {
-        return this.bookingsService.createBooking(bookingDto);
-    }
+  @Post()
+  create(@Body() bookingDto: CreateBookingDto) {
+    return this.bookingsService.createBooking(bookingDto);
+  }
 
-    @Get()
-    allBookings() {
-        return this.bookingsService.getAllBooking();
-    }
+  @Get()
+  allBookings() {
+    return this.bookingsService.getAllBooking();
+  }
 }
--- src/bookings/bookings.service.ts
+++ src/bookings/bookings.service.ts
@@ -1,20 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Booking } from './bookings.model';
-import { Model } from 'mongoose';
-import { CreateBookingDto } from './dto/create-booking.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Booking } from "./bookings.model";
+import { IBooking } from "src/helpers/interfaces";
+import { CreateBookingDto } from "./dto/create-booking.dto";
+import { UsersService } from "src/users/users.service";
+import { User } from "src/users/users.model";
 
 @Injectable()
 export class BookingsService {
-    constructor(@InjectModel(Booking.name) private bookingModel: Model<Booking>) {}
+  constructor(
+    @InjectModel(Booking.name) private bookingModel: Model<Booking>,
+    private readonly userService: UsersService
+  ) {}
 
-    async createBooking(dto: CreateBookingDto) {
-        const newBooking = await this.bookingModel.create(dto);
-        return newBooking;
+  private async validateBooking(dto: CreateBookingDto): Promise<IBooking> {
+    const isExistUser: User = await this.userService.findOneByEmail(dto.email);
+    if (isExistUser) {
+      const newBooking: Booking = await this.bookingModel.create(dto);
+      const updatedBooking: Booking = await this.bookingModel
+        .findByIdAndUpdate(
+          newBooking._id,
+          { owner: isExistUser._id, roles: [isExistUser.roles[0]] },
+          { new: true }
+        )
+        .exec();
+
+      return updatedBooking;
     }
 
-    async getAllBooking() {
-        const bookings = await this.bookingModel.find().exec(); // all bookings
-        return bookings;
-    }
+    return await this.bookingModel.create(dto);
+  }
+
+  async createBooking(dto: CreateBookingDto): Promise<IBooking> {
+    return await this.validateBooking(dto);
+  }
+
+  async getAllBooking() {
+    const bookings = await this.bookingModel.find().exec(); // all bookings
+    return bookings;
+  }
 }
